639346e7d74ad58f446d6a76a250fc07c688deb8
twoneis <sanjay29@sapura.de>
2022-05-19 19:45:13 +0200

    added ability to add and view new issues

--- Controllers/IssueController.cs
+++ Controllers/IssueController.cs
@@ -19,7 +19,36 @@ namespace project_tracker.Controllers
             IEnumerable<Issue> IssueList = _db.Issues.ToList();
             return View(IssueList); 
         }
+        //GET
+        public IActionResult Add() { 
+            return View(); 
+        }
 
-        public IActionResult Add() { return View(); }
+        public IActionResult Display(int? id) {
+            if(id == null) { return NotFound(); }
+            var issue = _db.Issues.Find(id);
+            if (issue == null) { return NotFound(); }
+            return View(issue);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Add(Issue obj)
+        {
+            _db.Issues.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Issue obj)
+        {
+            _db.Issues.Update(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction("Index");
+        }
     }
 }
